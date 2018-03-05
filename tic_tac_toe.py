af2026d5383ad6be88f3f479c2bebacad8c201d0
Raj <33696370+rajshah001@users.noreply.github.com>
2018-03-05 17:00:20 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,6 @@
 import time
 import random
+
 # asks for the sign from user
 print("Which sign do you want to have ? x or o")
 ask_sign = input()
@@ -30,16 +31,12 @@ def tic_tac():  # final display on which the result appears
 # saves the sign in variables
 if ask_sign == "x":
     user_sign = "x"
-    user_sign_1 = user_sign
     computer_sign = "o"
-    computer_sign_1 = computer_sign
 elif ask_sign == "o":
     user_sign = "o"
-    user_sign_1 = user_sign
     computer_sign = "x"
-    computer_sign_1 = computer_sign
 
-total = [10]
+total = []
 
 num_1 = " "
 num_2 = " "
@@ -51,11 +48,12 @@ num_7 = " "
 num_8 = " "
 num_9 = " "
 
-found = False
-while not False:
+while True:
+    
     print("Please enter your valid move: ")
     user_input = int(input())
-    if user_input in total:
+
+    if user_input in total or user_input > 9:
         print("Kindly enter a move which is not used.")
         continue
     # it is the logic for the implementation and registering of input the user gave.
@@ -63,113 +61,97 @@ while not False:
         total.append(user_input)
 
         if user_input == 1:
-            num_1 = user_sign_1
+            num_1 = user_sign
         elif user_input == 2:
-            num_2 = user_sign_1
+            num_2 = user_sign
         elif user_input == 3:
-            num_3 = user_sign_1
+            num_3 = user_sign
         elif user_input == 4:
-            num_4 = user_sign_1
+            num_4 = user_sign
         elif user_input == 5:
-            num_5 = user_sign_1
+            num_5 = user_sign
         elif user_input == 6:
-            num_6 = user_sign_1
+            num_6 = user_sign
         elif user_input == 7:
-            num_7 = user_sign_1
+            num_7 = user_sign
         elif user_input == 8:
-            num_8 = user_sign_1
+            num_8 = user_sign
         elif user_input == 9:
-            num_9 = user_sign_1
+            num_9 = user_sign
 
-    # checks for the tie.
-    if len(total) == 11:
-        print("THE GAME HAS BEEN TIED.")
-        print(tic_tac())
-        break
-    if num_1 == num_2 == num_3 == user_sign_1 or num_4 == num_5 == num_6 == user_sign_1 or num_7 == num_8 == num_9 == user_sign_1 or num_1 == num_4 == num_7 == user_sign_1 or \
-                                    num_2 == num_5 == num_8 == user_sign_1 or num_3 == num_6 == num_9 == user_sign_1 or num_1 == num_5 == num_9 == user_sign_1 or num_3 == num_5 == num_7 == user_sign_1:
+    if num_1 == num_2 == num_3 == user_sign or num_4 == num_5 == num_6 == user_sign or num_7 == num_8 == num_9 == user_sign or num_1 == num_4 == num_7 == user_sign or \
+                                    num_2 == num_5 == num_8 == user_sign or num_3 == num_6 == num_9 == user_sign or num_1 == num_5 == num_9 == user_sign or num_3 == num_5 == num_7 == user_sign:
         print("The user wins. \n ----- CONGRATULATIONS -----")
         print(tic_tac())
         break
 
-    if num_1 == num_2 == num_3 == computer_sign_1 or num_4 == num_5 == num_6 == computer_sign_1 or num_7 == num_8 == num_9 == computer_sign_1 or num_1 == num_4 == num_7 == computer_sign_1 or \
-                                    num_2 == num_5 == num_8 == computer_sign_1 or num_3 == num_6 == num_9 == computer_sign_1 or num_1 == num_5 == num_9 == computer_sign_1 or num_3 == num_5 == num_7 == computer_sign_1:
+    if num_1 == num_2 == num_3 == computer_sign or num_4 == num_5 == num_6 == computer_sign or num_7 == num_8 == num_9 == computer_sign or num_1 == num_4 == num_7 == computer_sign or \
+                                    num_2 == num_5 == num_8 == computer_sign or num_3 == num_6 == num_9 == computer_sign or num_1 == num_5 == num_9 == computer_sign or num_3 == num_5 == num_7 == computer_sign:
         print("The computer wins. \n Better luck next time. ")
         print(tic_tac())
         break
 
+    # checks for the tie.
+    if len(total) == 9:
+        print("THE GAME HAS BEEN TIED.")
+        print(tic_tac())
+        break
+
     # it is the logic for the implementation and registering of random move the computer gave.
-    if 1 < len(total) < 11:
-        while True:
-            ran_no = random.randint(1, 9)
-            if ran_no in total:
-                continue
-            elif ran_no not in total:
-                break
 
-        if ran_no not in total:
-            total.append(ran_no)
-            if ran_no == 1:
-                num_1 = computer_sign_1
+    while True:
+        ran_no = random.randint(1, 9)
+        if ran_no in total:
+            continue
+        elif ran_no not in total:
+            break
 
-            elif ran_no == 2:
-                num_2 = computer_sign_1
+    if ran_no not in total:
+        total.append(ran_no)
+        if ran_no == 1:
+            num_1 = computer_sign
 
-            elif ran_no == 3:
-                num_3 = computer_sign_1
+        elif ran_no == 2:
+            num_2 = computer_sign
 
-            elif ran_no == 4:
-                num_4 = computer_sign_1
+        elif ran_no == 3:
+            num_3 = computer_sign
 
-            elif ran_no == 5:
-                num_5 = computer_sign_1
+        elif ran_no == 4:
+            num_4 = computer_sign
 
-            elif ran_no == 6:
-                num_6 = computer_sign_1
+        elif ran_no == 5:
+            num_5 = computer_sign
 
-            elif ran_no == 7:
-                num_7 = computer_sign_1
+        elif ran_no == 6:
+            num_6 = computer_sign
 
-            elif ran_no == 8:
-                num_8 = computer_sign_1
+        elif ran_no == 7:
+            num_7 = computer_sign
 
-            elif ran_no == 9:
-                num_9 = computer_sign_1
+        elif ran_no == 8:
+            num_8 = computer_sign
 
-            if num_1 == num_2 == num_3 == user_sign_1 or num_4 == num_5 == num_6 == user_sign_1 or num_7 == num_8 == num_9 == user_sign_1 or num_1 == num_4 == num_7 == user_sign_1 or \
-                    num_2 == num_5 == num_8 == user_sign_1 or num_3 == num_6 == num_9 == user_sign_1 or num_1 == num_5 == num_9 == user_sign_1 or num_3 == num_5 == num_7 == user_sign_1:
-                print("The user wins. \n ----- CONGRATULATIONS -----")
-                print(tic_tac())
-                break
+        elif ran_no == 9:
+            num_9 = computer_sign
 
-            if num_1 == num_2 == num_3 == computer_sign_1 or num_4 == num_5 == num_6 == computer_sign_1 or num_7 == num_8 == num_9 == computer_sign_1 or num_1 == num_4 == num_7 == computer_sign_1 or \
-                    num_2 == num_5 == num_8 == computer_sign_1 or num_3 == num_6 == num_9 == computer_sign_1 or num_1 == num_5 == num_9 == computer_sign_1 or num_3 == num_5 == num_7 == computer_sign_1:
-                print("The computer wins. \n Better luck next time. ")
-                print(tic_tac())
-                break
 
-        elif ran_no in total:
-            if ran_no == 1:
-                num_3 = computer_sign_1
-                total.append(3)
-            elif ran_no == 9:
-                num_7 = computer_sign_1
-                total.append(7)
-            else:
-                num_5 = computer_sign_1
-                total.append(5)
+        if num_1 == num_2 == num_3 == user_sign or num_4 == num_5 == num_6 == user_sign or num_7 == num_8 == num_9 == user_sign or num_1 == num_4 == num_7 == user_sign or \
+                    num_2 == num_5 == num_8 == user_sign or num_3 == num_6 == num_9 == user_sign or num_1 == num_5 == num_9 == user_sign or num_3 == num_5 == num_7 == user_sign:
+            print("The user wins. \n ----- CONGRATULATIONS -----")
+            print(tic_tac())
+            break
 
-            if num_1 == num_2 == num_3 == user_sign_1 or num_4 == num_5 == num_6 == user_sign_1 or num_7 == num_8 == num_9 == user_sign_1 or num_1 == num_4 == num_7 == user_sign_1 or \
-                    num_2 == num_5 == num_8 == user_sign_1 or num_3 == num_6 == num_9 == user_sign_1 or num_1 == num_5 == num_9 == user_sign_1 or num_3 == num_5 == num_7 == user_sign_1:
-                print("The user wins. \n ----- CONGRATULATIONS -----")
-                print(tic_tac())
-                break
+        if num_1 == num_2 == num_3 == computer_sign or num_4 == num_5 == num_6 == computer_sign or num_7 == num_8 == num_9 == computer_sign or num_1 == num_4 == num_7 == computer_sign or \
+                    num_2 == num_5 == num_8 == computer_sign or num_3 == num_6 == num_9 == computer_sign or num_1 == num_5 == num_9 == computer_sign or num_3 == num_5 == num_7 == computer_sign:
+            print("The computer wins. \n Better luck next time. ")
+            print(tic_tac())
+            break
 
-            if num_1 == num_2 == num_3 == computer_sign_1 or num_4 == num_5 == num_6 == computer_sign_1 or num_7 == num_8 == num_9 == computer_sign_1 or num_1 == num_4 == num_7 == computer_sign_1 or \
-                    num_2 == num_5 == num_8 == computer_sign_1 or num_3 == num_6 == num_9 == computer_sign_1 or num_1 == num_5 == num_9 == computer_sign_1 or num_3 == num_5 == num_7 == computer_sign_1:
-                print("The computer wins. \n Better luck next time. ")
-                print(tic_tac())
-                break
+    else:
+        pass
+    
     print(tic_tac())
 
 time.sleep(4)
+
